f4e4e329d4c429a29ac9708d6cb1e6b16632f330
AlexSpohr <alexspohr436@gmail.com>
2024-02-23 22:10:25 -0300

    fix: cep replace

--- src/main/java/com/compassuol/sp/challenge/msaddress/domain/address/service/AddressService.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/address/service/AddressService.java
@@ -14,6 +14,7 @@ public class AddressService {
     private final AddressConsumerFeing addressConsumerFeing;
 
     public void saveAddress(String cep) {
+        cep = cep.replace("-", "");
         Address address = addressConsumerFeing.getAddressByCep(cep);
         addressRepository.save(address);
     }
