8b7ef22ec85ab7436f66597464e3b03538eb7440
AlexSpohr <alexspohr436@gmail.com>
2024-02-23 21:04:38 -0300

    feat: Address consumer Open Feing

--- src/main/java/com/compassuol/sp/challenge/msaddress/MsAddressApplication.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/MsAddressApplication.java
@@ -2,7 +2,9 @@ package com.compassuol.sp.challenge.msaddress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsAddressApplication {
 
